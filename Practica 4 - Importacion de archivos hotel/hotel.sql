8d5c01375154e0145cc8f39302a080c94a41fc95
santiago-nunez1409 <sastorga580@gmail.com>
2026-02-18 08:28:15 -0700

    Commit 4 - 18/02/26
    Santiago Nuñez Astorga y Jose Alejandro Gonzalez Sanchez

---
+++ Practica 4 - Importacion de archivos hotel/hotel.sql
@@ -0,0 +1,374 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-02-2026 a las 16:24:58
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `hotel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `atencion`
+--
+
+CREATE TABLE `atencion` (
+  `id_atencion` int(11) NOT NULL,
+  `id_reserva` int(11) NOT NULL,
+  `id_empleado` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `nota` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefono` varchar(30) NOT NULL,
+  `fecha_registro` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id_cliente`, `nombre`, `email`, `telefono`, `fecha_registro`) VALUES
+(1, 'Israel Osorio', 'israel.osorio@gmail.com', '6677284890', '2026-02-04'),
+(2, 'Luis Armando', 'luis.arm@gmail.com', '6672182090', '2026-02-06'),
+(3, 'Yosgart Salazar', 'yosgart.salazar@gmail.com', '6672523391', '2026-02-08'),
+(4, 'Axel Rosales', 'axel.rosales@gmail.com', '6670902331', '2026-02-10'),
+(5, 'Kenneth Gastelum', 'kenneth.gast@gmail.com', '6675254531', '2026-02-12');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `id_empleado` int(11) NOT NULL,
+  `id_hotel` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `puesto` varchar(30) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `empleado`
+--
+
+INSERT INTO `empleado` (`id_empleado`, `id_hotel`, `nombre`, `puesto`, `email`) VALUES
+(1, 3, 'Juan Lopez', 'Recepcionista', 'juan.lopez@gmail.com'),
+(2, 1, 'Ernulfo Rodriguez', 'Limpieza', 'ernulfo.rod@gmail.com'),
+(3, 2, 'gabriela gomez', 'Recepcionista', 'gabriela.gomez@gmail.com'),
+(4, 1, 'Juan Jose', 'chef', 'juan.joose@gmail.com'),
+(5, 3, 'Ernesto Sanchez', 'Gerente', 'ernesto.sanchez@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `habitacion`
+--
+
+CREATE TABLE `habitacion` (
+  `id_habitacion` int(11) NOT NULL,
+  `id_hotel` int(11) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `tipo` varchar(30) NOT NULL,
+  `precio_noche` double NOT NULL,
+  `estado` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `habitacion`
+--
+
+INSERT INTO `habitacion` (`id_habitacion`, `id_hotel`, `numero`, `tipo`, `precio_noche`, `estado`) VALUES
+(1, 1, 102, 'Estandar', 1200, 'Reservado'),
+(2, 1, 104, 'Deluxe', 1600, 'Sin Reservar'),
+(3, 2, 80, 'Doble', 1400, 'Sin Reservar'),
+(4, 2, 84, 'Suite', 2400, 'Sin Reservar'),
+(5, 3, 404, 'Individual', 950, 'Sin Reservar');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `hotel`
+--
+
+CREATE TABLE `hotel` (
+  `id_hotel` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `ciudad` varchar(50) NOT NULL,
+  `telefono` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `hotel`
+--
+
+INSERT INTO `hotel` (`id_hotel`, `nombre`, `ciudad`, `telefono`) VALUES
+(1, 'InHouse', 'Culiacan', '667 996 7609'),
+(2, 'Hotel La Riviera', 'Culiacan', '667 716 8955'),
+(3, 'Sleep Inn', 'Culiacan', '667 283 7000');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pago`
+--
+
+CREATE TABLE `pago` (
+  `id_pago` int(11) NOT NULL,
+  `id_reserva` int(11) NOT NULL,
+  `fecha_pago` date NOT NULL,
+  `monto` double NOT NULL,
+  `metodo` varchar(255) NOT NULL,
+  `referencia` varchar(50) NOT NULL,
+  `estado_pago` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reserva`
+--
+
+CREATE TABLE `reserva` (
+  `id_reserva` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_habitacion` int(11) NOT NULL,
+  `fecha_entrada` date NOT NULL,
+  `fecha_salida` date NOT NULL,
+  `num_huespedes` int(20) NOT NULL,
+  `estado_reserva` varchar(20) NOT NULL,
+  `fecha_creacion` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `reserva`
+--
+
+INSERT INTO `reserva` (`id_reserva`, `id_cliente`, `id_habitacion`, `fecha_entrada`, `fecha_salida`, `num_huespedes`, `estado_reserva`, `fecha_creacion`) VALUES
+(1, 1, 1, '2026-02-18', '2026-02-21', 1, 'vigente', '2026-02-18'),
+(2, 1, 3, '2026-02-20', '2026-02-25', 2, 'vigente', '2026-02-18'),
+(3, 2, 4, '2026-02-26', '2026-03-03', 4, 'vigente', '2026-02-18'),
+(4, 2, 3, '2026-02-19', '2026-02-25', 2, 'vigente', '2026-02-18'),
+(5, 3, 2, '2026-02-21', '2026-02-25', 3, 'vigente', '2026-02-18'),
+(6, 3, 1, '2026-02-21', '2026-02-28', 2, 'vigente', '2026-02-18'),
+(7, 4, 4, '2026-03-05', '2026-03-13', 5, 'vigente', '2026-02-18'),
+(8, 4, 3, '2026-03-07', '2026-03-10', 2, 'vigente', '2026-02-18'),
+(9, 5, 5, '2026-02-26', '2026-03-12', 1, 'vigente', '2026-02-18'),
+(10, 5, 2, '2026-03-30', '2026-04-05', 3, 'vigente', '2026-02-18');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reserva_servicio`
+--
+
+CREATE TABLE `reserva_servicio` (
+  `id_reserva` int(11) NOT NULL,
+  `id_servicio` int(11) NOT NULL,
+  `cantidad` int(50) NOT NULL,
+  `precio_unitario` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicio`
+--
+
+CREATE TABLE `servicio` (
+  `id_servicio` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `precio_base` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `atencion`
+--
+ALTER TABLE `atencion`
+  ADD PRIMARY KEY (`id_atencion`),
+  ADD KEY `id_reserva` (`id_reserva`),
+  ADD KEY `id_empleado` (`id_empleado`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`id_empleado`),
+  ADD KEY `id_hotel` (`id_hotel`);
+
+--
+-- Indices de la tabla `habitacion`
+--
+ALTER TABLE `habitacion`
+  ADD PRIMARY KEY (`id_habitacion`),
+  ADD KEY `id_hotel` (`id_hotel`);
+
+--
+-- Indices de la tabla `hotel`
+--
+ALTER TABLE `hotel`
+  ADD PRIMARY KEY (`id_hotel`);
+
+--
+-- Indices de la tabla `pago`
+--
+ALTER TABLE `pago`
+  ADD PRIMARY KEY (`id_pago`),
+  ADD KEY `id_reserva` (`id_reserva`);
+
+--
+-- Indices de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD PRIMARY KEY (`id_reserva`),
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `id_habitacion` (`id_habitacion`);
+
+--
+-- Indices de la tabla `reserva_servicio`
+--
+ALTER TABLE `reserva_servicio`
+  ADD KEY `id_reserva` (`id_reserva`),
+  ADD KEY `id_servicio` (`id_servicio`);
+
+--
+-- Indices de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD PRIMARY KEY (`id_servicio`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `atencion`
+--
+ALTER TABLE `atencion`
+  MODIFY `id_atencion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `habitacion`
+--
+ALTER TABLE `habitacion`
+  MODIFY `id_habitacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `hotel`
+--
+ALTER TABLE `hotel`
+  MODIFY `id_hotel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `pago`
+--
+ALTER TABLE `pago`
+  MODIFY `id_pago` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  MODIFY `id_servicio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `atencion`
+--
+ALTER TABLE `atencion`
+  ADD CONSTRAINT `atencion_ibfk_1` FOREIGN KEY (`id_reserva`) REFERENCES `reserva` (`id_reserva`),
+  ADD CONSTRAINT `atencion_ibfk_2` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`);
+
+--
+-- Filtros para la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`id_hotel`) REFERENCES `hotel` (`id_hotel`);
+
+--
+-- Filtros para la tabla `habitacion`
+--
+ALTER TABLE `habitacion`
+  ADD CONSTRAINT `habitacion_ibfk_1` FOREIGN KEY (`id_hotel`) REFERENCES `hotel` (`id_hotel`);
+
+--
+-- Filtros para la tabla `pago`
+--
+ALTER TABLE `pago`
+  ADD CONSTRAINT `pago_ibfk_1` FOREIGN KEY (`id_reserva`) REFERENCES `reserva` (`id_reserva`);
+
+--
+-- Filtros para la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD CONSTRAINT `reserva_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`),
+  ADD CONSTRAINT `reserva_ibfk_2` FOREIGN KEY (`id_habitacion`) REFERENCES `habitacion` (`id_habitacion`);
+
+--
+-- Filtros para la tabla `reserva_servicio`
+--
+ALTER TABLE `reserva_servicio`
+  ADD CONSTRAINT `reserva_servicio_ibfk_1` FOREIGN KEY (`id_reserva`) REFERENCES `reserva` (`id_reserva`),
+  ADD CONSTRAINT `reserva_servicio_ibfk_2` FOREIGN KEY (`id_servicio`) REFERENCES `servicio` (`id_servicio`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
